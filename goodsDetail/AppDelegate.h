a241112bbce8733a457bb1d28c87ff97c5de2b8a
zhengbj <157272494@qq.com>
2015-09-14 16:15:27 +0800

    init

---
+++ goodsDetail/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  goodsDetail
+//
+//  Created by jack steven on 15/9/14.
+//  Copyright (c) 2015年 zhengbj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ goodsDetail/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  goodsDetail
+//
+//  Created by jack steven on 15/9/14.
+//  Copyright (c) 2015年 zhengbj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
